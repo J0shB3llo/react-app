a689cbbabbcb4f6ef40f3f2b0ad18e62a4a6dc02
George Bello <georgebello479@gmail.com>
2021-09-09 17:18:58 -0400

    Subiendo archivos

--- src/App.js
+++ src/App.js
@@ -2,116 +2,116 @@ import React, { Component } from "react";
 import compose from "lodash/flowRight";
 import { HashRouter as Router, withRouter, Link } from "react-router-dom";
 
-// import { EightBaseAppProvider } from '@8base/app-provider';
+import { EightBaseAppProvider } from '@8base/app-provider';
 
-// import gql from "graphql-tag";
-// import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 
 import "todomvc-app-css/index.css";
 import "./App.css";
 
 
-// const TODO_LIST_QUERY = gql`
-//   query TodoList {
-//     todosList(orderBy: [completed_ASC, createdAt_DESC]) {
-//       items {
-//         id
-//         text
-//         completed
-//       }
-//     }
-//   }
-// `;
+const TODO_LIST_QUERY = gql`
+  query TodoList {
+    todosList(orderBy: [completed_ASC, createdAt_DESC]) {
+      items {
+        id
+        text
+        completed
+      }
+    }
+  }
+`;
 
-// const withTodos = graphql(TODO_LIST_QUERY, {
-//   props: ({ data: { todosList }}) => {
-//     let todos = []
-//     if (todosList) {
-//       todos = todosList.items;
-//     }
-//     return {
-//       todos
-//     };
-//   },
-// });
-
-
-// const CREATE_TODO_MUTATION = gql`
-//   mutation TodoCreate($data: TodoCreateInput!) {
-//     todoCreate(data: $data) {
-//       id
-//       text
-//       completed
-//     }
-//   }
-// `;
-
-// const withCreateTodo = graphql(CREATE_TODO_MUTATION, {
-//   props: ({ mutate }) => ({
-//     createTodo: ({ text }) => {
-//       mutate({
-//         variables: { data: { text, completed: false } },
-//         refetchQueries: [{ query: TODO_LIST_QUERY }]
-//       });
-//     }
-//   })
-// });
+const withTodos = graphql(TODO_LIST_QUERY, {
+  props: ({ data: { todosList }}) => {
+    let todos = []
+    if (todosList) {
+      todos = todosList.items;
+    }
+    return {
+      todos
+    };
+  },
+});
 
 
-// const TOGGLE_TODO_MUTATION = gql`
-//   mutation TodoToggle($id: ID!, $completed: Boolean!) {
-//     todoUpdate(filter: { id: $id }, data: {
-//         completed: $completed
-//     }) {
-//       id
-//       text
-//       completed
-//     }
-//   }
-// `;
+const CREATE_TODO_MUTATION = gql`
+  mutation TodoCreate($data: TodoCreateInput!) {
+    todoCreate(data: $data) {
+      id
+      text
+      completed
+    }
+  }
+`;
 
-// const withToggleTodo = graphql(TOGGLE_TODO_MUTATION, {
-//   props: ({ mutate }) => ({
-//     toggleTodo: ({ id, completed }) => {
-//       mutate({
-//         variables: { id, completed },
-//         refetchQueries: [{ query: TODO_LIST_QUERY }]
-//       });
-//     }
-//   })  
-// });
+const withCreateTodo = graphql(CREATE_TODO_MUTATION, {
+  props: ({ mutate }) => ({
+    createTodo: ({ text }) => {
+      mutate({
+        variables: { data: { text, completed: false } },
+        refetchQueries: [{ query: TODO_LIST_QUERY }]
+      });
+    }
+  })
+});
 
-// const withToggleAllTodos = graphql(TOGGLE_TODO_MUTATION, {
-//   props: ({ mutate, ownProps: { todos }}) => ({
-//     toggleAllTodos: ({ completed }) => {      
-//       todos.forEach((todo) => {
-//         mutate({
-//           variables: { id: todo.id, completed },
-//           refetchQueries: [{ query: TODO_LIST_QUERY }]
-//         });        
-//       });      
-//     }
-//   })
-// });
 
-// const DELETE_TODO_MUTATION = gql`
-//   mutation TodoDelete($id: ID!) {
-//     todoDelete(filter: { id: $id }) {
-//       success
-//     }
-//   }
-// `;
+const TOGGLE_TODO_MUTATION = gql`
+  mutation TodoToggle($id: ID!, $completed: Boolean!) {
+    todoUpdate(filter: { id: $id }, data: {
+        completed: $completed
+    }) {
+      id
+      text
+      completed
+    }
+  }
+`;
 
-// const withRemoveTodo = graphql(DELETE_TODO_MUTATION, {
-//   props: ({ mutate }) => ({
-//     removeTodo: ( id ) => {
-//       mutate({
-//         variables: { id },
-//         refetchQueries: [{ query: TODO_LIST_QUERY }]
-//       });
-//     }
-//   })  
-// });
+const withToggleTodo = graphql(TOGGLE_TODO_MUTATION, {
+  props: ({ mutate }) => ({
+    toggleTodo: ({ id, completed }) => {
+      mutate({
+        variables: { id, completed },
+        refetchQueries: [{ query: TODO_LIST_QUERY }]
+      });
+    }
+  })  
+});
+
+const withToggleAllTodos = graphql(TOGGLE_TODO_MUTATION, {
+  props: ({ mutate, ownProps: { todos }}) => ({
+    toggleAllTodos: ({ completed }) => {      
+      todos.forEach((todo) => {
+        mutate({
+          variables: { id: todo.id, completed },
+          refetchQueries: [{ query: TODO_LIST_QUERY }]
+        });        
+      });      
+    }
+  })
+});
+
+const DELETE_TODO_MUTATION = gql`
+  mutation TodoDelete($id: ID!) {
+    todoDelete(filter: { id: $id }) {
+      success
+    }
+  }
+`;
+
+const withRemoveTodo = graphql(DELETE_TODO_MUTATION, {
+  props: ({ mutate }) => ({
+    removeTodo: ( id ) => {
+      mutate({
+        variables: { id },
+        refetchQueries: [{ query: TODO_LIST_QUERY }]
+      });
+    }
+  })  
+});
 
 class Header extends Component {
   state = { text: "" };
@@ -139,20 +139,20 @@ class Header extends Component {
   }
 }
 
-// Header = withCreateTodo(Header);
+Header = withCreateTodo(Header);
 
 
 class Main extends Component {
+
 
   render() {
     const {
       todos,
       toggleTodo,
+      toggleAllTodos,
       removeTodo,
-      toggleAllTodos, // remove this
       location
     } = this.props;
-
     return todos && todos.length ? (
       <section className="main">
         <input
@@ -209,10 +209,10 @@ class Main extends Component {
 
 Main = compose(
   withRouter,
-  // withTodos,
-  // withToggleTodo,
-  // withToggleAllTodos,
-  // withRemoveTodo
+  withTodos,
+  withToggleTodo,
+  withToggleAllTodos,
+  withRemoveTodo
 )(Main);
 
 class Footer extends Component {
@@ -261,102 +261,25 @@ class Footer extends Component {
 
 Footer = compose(
   withRouter,
-  // withTodos  
+  withTodos  
 )(Footer);
 
-// const ENDPOINT_URL = 'YOUR_8BASE_ENDPOINT_URL';
+const ENDPOINT_URL = 'https://api.8base.com/cjsrk2gxq000d01s06ye4s4vz';
 
 class App extends Component {
-
-  // Remove everything from here until...
-  constructor() {
-    super();
-    const todos = [
-      {
-        id: '1',
-        text: 'Todo 1',
-        completed: false
-      },
-      {
-        id: '2',
-        text: 'Todo 2',
-        completed: false
-      },
-      {
-        id: '3',
-        text: 'Todo 3',
-        completed: true
-      }
-    ];
-
-    this.state = { todos };
-  }
-
-  toggleAllTodos = ({ completed }) => {
-    const { todos } = this.state;
-    todos.forEach((todo) => {
-      todo.completed = completed;
-    });
-    this.setState({ todos });
-  }
-
-  toggleTodo = ({ id, completed }) => {
-    const { todos } = this.state;
-    todos.forEach((todo) => {
-      if (todo.id === id) {
-        todo.completed = completed;
-      }
-    });
-    this.setState({ todos });
-  }
-
-  removeTodo = (id) => {
-    let { todos } = this.state;
-    todos = todos.filter(( todo ) => {
-      return todo.id !== id;
-    });
-    this.setState({ todos });
-  }
-
-  createTodo = ({ text }) => {
-    let { todos } = this.state;
-    const lastTodo = todos[todos.length - 1];
-    let newTodoId = 1; 
-    if (lastTodo) {
-      newTodoId = parseInt(lastTodo.id, 10) + 1;
-    }
-    todos.push({
-      id: newTodoId.toString(),
-      text,
-      completed: false
-    });
-    this.setState({ todos });
-  }
-
-  // ...until here
 
   render() {
     return (
       <Router>
-        {/* Uncomment the code below */}
-        {/*<EightBaseAppProvider uri={ENDPOINT_URL} >
-          {({ loading }) => loading ? <div>"Loading..."</div> : ( */}
+        <EightBaseAppProvider uri={ENDPOINT_URL} >
+          {({ loading }) => loading ? <div>"Loading..."</div> : (
             <div className="todoapp">
-              <Header 
-                  createTodo={ this.createTodo }            // Remove this
-              />
-              <Main 
-                  todos={ this.state.todos }                // Remove this                  
-                  toggleTodo={ this.toggleTodo }            // Remove this
-                  removeTodo={ this.removeTodo }            // Remove this
-                  toggleAllTodos={ this.toggleAllTodos }    // Remove this
-                  />         
-              <Footer 
-                  todos={ this.state.todos }                // Remove this
-              />
+              <Header />
+              <Main />
+              <Footer />
             </div>
-          {/*})}
-        </EightBaseAppProvider>*/}
+          )}
+        </EightBaseAppProvider>
       </Router>
     );
   }
